cf43d04c1059feb14cf15ef863e49ab370c7cab2
IROHA21 <mouniraitaissa1@gmail.com>
2025-06-26 12:16:50 +0300

    finished task 2

--- task_02.py
+++ task_02.py
@@ -1,8 +1,18 @@
-def coincidence(lis, ran) :
-    for i in ran :
-        print (lis[i-1])
+def coincidence(lis=None, ran=None):
+    if lis is None or ran is None:
+        return []
 
+    start = ran.start
+    stop = ran.stop
+    result = []
 
+    for i in lis:
+        if isinstance(i, (int, float)) :
+            if start <= i < stop:
+                result.append(i)
+    return result
 
 
 print(coincidence([1, 2, 3, 4, 5], range(3, 6)))             # => [3, 4, 5]
+print(coincidence())                                         # => []
+print(coincidence([None, 1, 'foo', 4, 2, 2.5], range(1, 4))) # => [1, 2, 2.5]
