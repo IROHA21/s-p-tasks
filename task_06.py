45578111ac16a6b3c30f861dc4427ad8d50d24e9
IROHA21 <mouniraitaissa1@gmail.com>
2025-08-01 14:24:11 +0300

    finished task6

--- task_06.py
+++ task_06.py
@@ -1,14 +1,35 @@
+class WrongNumberOfPlayersError(Exception):
+    pass
+
+
+class NoSuchStrategyError(Exception):
+    pass
+
+
 def rps_game_winner(list):
-    if not list :
-        pass
+    if len(list) != 2:
+        raise WrongNumberOfPlayersError
 
-    for elem in list :
-        print(elem)
-        print(type(elem))
+    newdict = dict(list)
+    rules = {"R": "S", "P": "R", "S": "P"}
 
+    for move in newdict.values():
+        if move not in {"R", "P", "S"}:
+            raise NoSuchStrategyError
 
-    return len(list)
+    (player1, move1), (player2, move2) = newdict.items()
 
+    if move1 == move2:
+        return f"{player1} {move1}"
+    elif rules[move1] == move2:
+        return f"{player1} {move1}"
+    else:
+        return f"{player2} {move2}"
 
+print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
 
-print(rps_game_winner([['player1','P'],['player2','S']]))
+print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))
+
+print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))
+
+print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))
