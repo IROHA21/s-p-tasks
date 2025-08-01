3022e3245778652b49c29c4b6ec4c5bdaeff2592
pc2 <mouniraitaissa1@gmail.com>
2025-08-01 12:42:44 +0300

    progress on task 6

--- task_06.py
+++ task_06.py
@@ -1,3 +1,14 @@
 def rps_game_winner(list):
     if not list :
         pass
+
+    for elem in list :
+        print(elem)
+        print(type(elem))
+
+
+    return len(list)
+
+
+
+print(rps_game_winner([['player1','P'],['player2','S']]))
